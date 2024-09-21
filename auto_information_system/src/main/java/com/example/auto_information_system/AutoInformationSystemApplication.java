1b7379a087f2db522ab185e9e0b2738b4b11f38c
Kilka0s0Tomatami <karatel25800@gmail.com>
2024-09-21 18:34:03 +0300

    start

---
+++ auto_information_system/src/main/java/com/example/auto_information_system/AutoInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.auto_information_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoInformationSystemApplication.class, args);
+	}
+
+}
---
+++ auto_information_system/src/test/java/com/example/auto_information_system/AutoInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto_information_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
