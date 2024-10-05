924d00b45f1c651d760d24752b4ed0adb3a10361
aapavlov <ppavlov.ar@yandex.ru>
2024-10-05 23:22:30 +0300

    medickbag

---
+++ auto_information_system/src/main/java/com/example/auto_information_system/controllers/LibrarianRestController.java
@@ -0,0 +1,8 @@
+package com.example.auto_information_system.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LibrarianRestController {
+    
+}
